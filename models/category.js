2e3fd88d05bdc5f79ca151b9fdfaee558ddc4eca
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-10-17 22:56:02 +0500

    initial

--- models/category.js
+++ models/category.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const crypto = require('crypto');
+// function generateShortId() {
+//   const shortId = new mongoose.mongo.ObjectId().toString().slice(0,10);
+//   const id =`new ObjectId("${shortId}")`
+//   console.log(id);
+//   return shortId;
+// }
 const CategorySchema = new Schema(
   {
+  //  _id: {
+  //     type: mongoose.mongo.ObjectId,
+  //     default:generateShortId(),
+  //     required: true,
+  //   },
     name: {
       type: String,
       trim: true,
